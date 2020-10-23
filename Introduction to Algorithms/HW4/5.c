5af760e4ef03862bcd91b5c1a4220db0e5e937c3
gaoway@mail.ustc.edu.cn <2085438633@qq.com>
2020-10-23 09:58:58 +0800

    update HW4 work

---
+++ Introduction to Algorithms/HW4/5.c
@@ -0,0 +1,66 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void find_max(int* data, int start, int end, int* maxnum, int* counter) {
+//	int temp_maxnum = 0, temp_counter = 0;
+//	if (start == end) {
+//		temp_maxnum = data[start];
+//		temp_counter = 1;
+//	}
+//	else if (start > end) {
+//		temp_maxnum = -1;
+//		temp_counter = 1;
+//	}
+//	else {
+//		for (int i = start; i <= end; ++i) {
+//			if (data[i] > temp_maxnum) {
+//				temp_counter = 1;
+//				temp_maxnum = data[i];
+//			}
+//			else if (data[i] == temp_maxnum) {
+//				temp_counter++;
+//			}
+//		}
+//	}	
+//	*maxnum = temp_maxnum;
+//	*counter = temp_counter;
+//}
+//
+//int print_max(int* data, int n_day, int k_day) {
+//	int maxnum = 0, counter = 0;
+//	find_max(data, 0, k_day-1, &maxnum, &counter);
+//	printf("%d ", maxnum);
+//	for (int i = k_day; i < n_day; ++i) {
+//		int inarray = data[i];
+//		int outarray = data[i - k_day];
+//		if (outarray == maxnum) {
+//			if (counter > 1) {
+//				counter--;
+//			}
+//			else if (counter == 1) {
+//				find_max(data, i - k_day + 1, i-1, &maxnum, &counter);
+//			}
+//		}
+//		if (inarray == maxnum) {
+//			counter++;
+//		}
+//		else if (inarray > maxnum) {
+//			maxnum = inarray;
+//			counter = 1;
+//		}
+//		printf("%d ", maxnum);
+//	}
+//	return 0;
+//}
+//
+//int main() {
+//	int n_day = 0, k_day = 0;
+//	scanf("%d %d", &n_day, &k_day);
+//	int* data = (int*)malloc(sizeof(int) * n_day);
+//	for (int i = 0; i < n_day; ++i) {
+//		scanf("%d", data + i);
+//	}
+//	print_max(data, n_day, k_day);
+//	return 0;
+//}
---
+++ Introduction to Algorithms/HW4/data_base.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+	Data_Base/main.c
+	(C) 2020 USTC_GAOWAY
+*/
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// use hash list to memory the data
+//Use the divide as a hash function. the DIVIDE number select 40000 ,when I use 8501, time is out of limit
+const int MAX_NUM = 40000;
+//link list to memory the same number of hash function
+typedef struct hashptr{
+	int id;
+	int num;
+	hashptr* next;
+}hashptr ;
+
+hashptr* data[MAX_NUM];
+
+void Insert_Hash_Data(int id, int num) {
+	hashptr* insert = new hashptr;		//when I use `hashptr insert, something wrong happens, maybe bcs it in the stack`
+	insert->id = id;
+	insert->num = num;
+	insert->next = NULL;
+	hashptr* ptr = data[num % MAX_NUM];
+	if (ptr == NULL) {
+		data[num % MAX_NUM] = insert;
+		return;
+	}
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	
+	ptr->next = insert;
+}
+
+int Find_Hash_Data(int num) {
+	hashptr* ptr = data[num % MAX_NUM];
+	while (ptr != NULL) {
+		if (ptr->num == num)
+			return ptr->id;
+		else
+			ptr = ptr->next;
+	}
+	return -1;
+}
+
+int main() {
+
+	memset(data, NULL, sizeof(hashptr*) * MAX_NUM);
+	char *title = (char *)malloc(sizeof(char) * 12);
+	scanf("%s", title);			//read the whole string and compare the first char limit time
+	while (title[0] != 'E') {
+		
+		if (title[0] == 'I') {
+			int id = 0, num = 0;
+			scanf("%d %d", &id,&num);
+			 Insert_Hash_Data(id, num);
+		}
+		else if (title[0] == 'F') {
+			int num = 0;
+			scanf("%d", &num);
+			printf("%d\n",Find_Hash_Data(num));
+		}
+		scanf("%s", title);
+	}
+	return 0;
+}
